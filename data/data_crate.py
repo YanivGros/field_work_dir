8303c974ce970948b7a9990d564cd094b99cef49
YanivGros <qywoec@gmail.com>
2023-08-16 09:20:30 +0300

    cmd verion

--- data/data_crate.py
+++ data/data_crate.py
@@ -2,41 +2,40 @@ import numpy as np
 import pandas as pd
 
 
-def create_data(num_bits, num_samples, max_number, numbers_to_divide_by: list, save=False, use_old_data=False) -> pd.DataFrame:
+def create_data(num_bits, num_samples, max_number, numbers_to_divide_by: list, modules: list) -> pd.DataFrame:
     """
     :param num_bits: The number of bits to represent the input
     :param num_samples: The number of samples to create
     :param max_number: The maximum number to represent in the input
     :param numbers_to_divide_by: The numbers to divide by (the output)
-    :param save: Whether to save the data to a file
-    :param use_old_data: Whether to use old data from a file
+    :param modules: The modules that build the numbers to divide by
     :return: A dataframe with the input and output
     """
-    if use_old_data:
-        try:
-            df = pd.read_csv(f"data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
-            print(
-                f"loaded data from file data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
-            return df
-        except FileNotFoundError:
-            print(
-                f"could not find file data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
-            print("exiting...")
-            exit(1)
+    try:
+        df = pd.read_csv(f"data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
+        print(f"loaded data from file data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
+        return df
+    except FileNotFoundError:
+        print(f"could not find file data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv")
+        print("creating new data")
+
     assert max(numbers_to_divide_by) * max_number < 2 ** (num_bits - 1), "not enough bits to represent all numbers"
     decimal_numbers = np.random.randint(low=0, high=max_number, size=num_samples) * np.random.choice(
         numbers_to_divide_by, num_samples)
-    binary_numbers = np.unpackbits(decimal_numbers.view(np.uint8)).reshape(-1, num_bits)
-    binary_numbers = binary_numbers.astype(int)
+    # binary_numbers = np.unpackbits(decimal_numbers.view(np.uint8)).reshape(-1, num_bits)
+    binary_repr_func = np.vectorize(lambda num: list(map(int, np.binary_repr(num).zfill(num_bits))), otypes=[np.ndarray])
+    binary_numbers = np.array(binary_repr_func(decimal_numbers).tolist())
+    # binary_numbers = binary_numbers.astype(int)
     df = pd.DataFrame(binary_numbers)
-    df.columns = [f'input_{i}' for i in range(num_bits)]
+    df.columns = [f'input_{i}' for i in range(num_bits, 0, -1)]
     df['decimal'] = decimal_numbers
     for divider in numbers_to_divide_by:
         df[f"divide_by_{str(divider)}"] = (decimal_numbers % divider == 0).astype(int)
-    if save:
-        df.to_csv(f"data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv", index=False)
+    for divider in modules:
+        df[f"m_divide_by_{str(divider)}"] = (decimal_numbers % divider == 0).astype(int)
+    df.to_csv(f"data_max_{max_number}_sample_{num_samples}_first_divider_{numbers_to_divide_by}.csv", index=False)
     return df
 
 
 if __name__ == '__main__':
-    create_data(32, 10000, 100000, [4, 6, 9], save=True)
+    create_data(32, 10000, 100000, [4, 6, 9], modules=[2, 3])
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # This is a sample Python script.
+import argparse
 import datetime
 import itertools
 import os
+import threading
 import time
+from timeit import timeit
 
 import keras.metrics
 import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from utility import *
+from ann_visualizer.visualize import ann_viz
+from networkx.algorithms.community import girvan_newman
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -19,6 +25,7 @@ from keras.backend import clear_session
 from pandas import DataFrame
 
 from sklearn.model_selection import train_test_split
+import community
 
 from numpy.random import default_rng
 import bisect
@@ -28,8 +35,159 @@ from data.data_crate import create_data
 rng = default_rng(seed=42)
 
 
-# Yuval
-# todo - add dipper (2 layers) networks and more layers.
+def MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model: Sequential, layers, num_round=10, epoch_per_round=10):
+    accuracy_all_train = []
+    accuracy_all_test = []
+    accuracy_train = {}
+    accuracy_test = {}
+    for divider in numbers_to_divide_by:
+        accuracy_train[divider] = []
+        accuracy_test[divider] = []
+    for i in range(num_round):
+        for j, number in enumerate(numbers_to_divide_by):
+            cur_train = y_train.iloc[:, j]
+            cur_test = y_test.iloc[:, j]
+            model.fit(X_train, cur_train, validation_data=[X_test, cur_test], epochs=epoch_per_round, verbose=0, shuffle=True,
+                      use_multiprocessing=True, workers=4)
+            accuracy_all_train.extend(model.history.history['binary_accuracy'])
+            accuracy_all_test.extend(model.history.history['val_binary_accuracy'])
+            accuracy_train[number].extend(model.history.history['binary_accuracy'])
+            accuracy_test[number].extend(model.history.history['val_binary_accuracy'])
+        print(f'round {i} out of {num_round}')
+    plt.plot(accuracy_all_train)
+    plt.plot(accuracy_all_test)
+    plt.title(f'MVG, layers {layers}, round epoch {epoch_per_round}, divider {numbers_to_divide_by}')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'MVG, layers {layers}, round epoch {epoch_per_round}.png')
+    plt.show()
+    for divider in numbers_to_divide_by:
+        plt.plot(accuracy_train[divider], label=f'train {divider}')
+    plt.title(f'MVG, layers {layers}, round epoch {epoch_per_round}, {numbers_to_divide_by}, separate graphs')
+    plt.legend()
+    plt.savefig(f'MVG, layers {layers}, round epoch {epoch_per_round}, {numbers_to_divide_by}, separate graphs.png')
+    plt.show()
+
+
+def FG(X_train, y_train, X_test, y_test, divider, model: Sequential, layers, iterations=100):
+    model.fit(X_train, y_train, epochs=iterations, validation_data=(X_test, y_test), shuffle=True, use_multiprocessing=True, workers=4, )
+    accuracy_all_train = model.history.history['binary_accuracy']
+    accuracy_all_test = model.history.history['val_binary_accuracy']
+    if accuracy_all_test[-1] > 0.9:
+        point_where_accuracy_is_09 = bisect.bisect_left(accuracy_all_test, 0.9)
+        print(f'point where accuracy is 0.9 is {point_where_accuracy_is_09}')
+        plt.plot(point_where_accuracy_is_09, 0.9, 'ro')
+        plt.text(point_where_accuracy_is_09, 0.9, f'({point_where_accuracy_is_09}, 0.9)')
+    plt.plot(accuracy_all_train)
+    plt.plot(accuracy_all_test)
+    plt.title(f'FG, Divider {divider}, layers {layers}')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    if accuracy_all_test[-1] > 0.9:
+        plt.legend(['0.9', 'train', 'test'], loc='upper left')
+    else:
+        plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'FG, Divider {divider}, layers {layers}.png')
+    plt.show()
+    plt.clf()
+    print(f'FG, Divider {divider}, layers {layers}, accuracy {accuracy_all_test[-1]}')
+    return accuracy_all_train, accuracy_all_test
+
+
+def evaluate_model(model: Sequential, dividers, learning_type):
+    ann_viz(model, title=f"{learning_type}, Layers {len(model.layers), dividers}, dividers inputs")
+    # plt.savefig(f"{learning_type}, Layers {len(model.layers)}, {dividers} inputs.png")
+    G = convert_to_graph(model)
+    # partition = girvan_newman(G)
+    # partition = community.best_partition(G)
+    # modularity = community.modularity(partition, G)
+    # nx.draw(G)
+    # plt.show()
+
+
+def FG_all(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, layers, iterations=100):
+    accuracy_all_test = []
+    accuracy_all_train = []
+    for col in y_train.columns:
+        temp_train, temp_test = FG(X_train, y_train[col], X_test, y_test[col], col, model, layers, iterations=iterations)
+        accuracy_all_train.extend(temp_train)
+        accuracy_all_test.extend(temp_test)
+    plt.plot(accuracy_all_train)
+    plt.plot(accuracy_all_test)
+    plt.title(f'FG, Dividers {numbers_to_divide_by}, layers {layers}, full learning')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'FG, Dividers {numbers_to_divide_by}, layers {layers}, full learning.png')
+    plt.show()
+
+
+def clean_data(df: DataFrame):
+    mean = df['decimal'].mean()
+    var = df['decimal']
+    df.drop((df[df['decimal'] > mean + 2 * np.sqrt(var)]).index, inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.to_csv('data_after_cleaning.csv', index=False)
+    largest_bit = np.ceil(np.log2(df['decimal'].max() + 1))
+    print(f"amount of unique values: {df['decimal'].nunique()}, total amount of values: {len(df['decimal'])}")
+    print(f'largest bit in use is {largest_bit}')
+
+    # print some statistics about the data
+    print(df['decimal'].describe())
+    print(df.filter(regex='^divide_by').describe())
+    print(df.filter(regex='^m_divide_by').describe())
+
+
+def main():
+    args = crate_parser()
+    num_inputs = args.num_inputs
+    data_size = args.data_size
+    max_number = args.max_number
+    learning_type = args.learning_type
+    iterations_fg = args.iterations_fg
+    iterations_each_round_mvg = args.iterations_each_round_mvg
+    amount_of_rounds_mvg = args.amount_of_rounds_mvg
+    layers = args.layers
+    modules = [3, 7, 11]
+    numbers_to_divide_by = get_all_premonition_mult(modules, amount_of_elements=2)
+
+    df: DataFrame = create_data(num_inputs, data_size, max_number, numbers_to_divide_by, modules)
+    clean_data(df)
+
+    print_with_separator("Data Created - Starting to train")
+    # plt.hist(df['decimal'], bins=100)
+    # plt.title('decimal distribution')
+    # plt.savefig('decimal distribution.png')
+    # plt.show()
+    X = df.filter(regex='^input')
+    y = df.filter(regex='^divide_by')
+    y_m = df.filter(regex='^m_divide_by')
+    # split the data into training and testing
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # change to results folder and create a folder with the current date and change to it
+    # crate_and_cd_to_dir("results")
+    # crate_and_cd_to_dir(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+
+    model = model_crate(num_inputs, layers)
+    if learning_type == 'MVG':
+        MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, layers, num_round=amount_of_rounds_mvg, epoch_per_round=iterations_each_round_mvg)
+        # evaluate_model(model, numbers_to_divide_by, 'MVG')
+    elif learning_type == 'FG':
+        FG_all(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, layers, iterations=iterations_fg)
+
+
+if __name__ == '__main__':
+    time = timeit(main, number=1)
+    print(f'time to run main: {time}')
+
+#     TODO:
+#     layers 3 - 10
+#     switch rate 2 - 20
+#     modularity
+#     start from one number
+#
+#      Add dipper (2 layers) networks and more layers.
 # todo - try 3,5,7 and 3,5,11 and 2,5,7 (multiply of those number).
 # todo - learn until near saturation and than swap.
 
@@ -39,184 +197,45 @@ rng = default_rng(seed=42)
 # TODO - check what is minimal layers needed to succeed in validation.
 # TODO - check how that history is saved.
 # TODO - girvan newman algorithm
+# TODO - multicalssfication - multi output - try with loss
+# todo - other goals.
+# todo - try wider neworks
+# todo - add L1
+# sequnail lerning of all goals in fg
+# find sd and mean of fg
+# print number of clsuter in fg
+# squre the edges (weights)
 
+# ---- history ----
 
-def convert_to_graph(model):
-    G = nx.Graph()
-    for layer in model.layers:
-        weights = layer.get_weights()
-        if len(weights) > 0:
-            # Get the weights and biases from the layer
-            W, b = weights[0], weights[1]
-            num_nodes_prev, num_nodes = W.shape
+# for layers in range(5, 6):
+#     clear_session()
+#     accuracy_all_test = []
+#     accuracy_all_train = []
+#     model = model_crate(num_inputs, layers)
+#     for col in y_train.columns:
+#         temp_train, temp_test = FG(X_train, y_train[col], X_test, y_test[col], col, model, layers, iterations=100)
+#         accuracy_all_train.extend(temp_train)
+#         accuracy_all_test.extend(temp_test)
+#     plt.plot(accuracy_all_train)
+#     plt.plot(accuracy_all_test)
+#     # add regrasion line for the test data to see if it is converging or not
+#     m, b = np.polyfit(np.arange(len(accuracy_all_test)), accuracy_all_test, 1)
+#     plt.plot(np.arange(len(accuracy_all_test)), m * np.arange(len(accuracy_all_test)) + b)
+#     # plt.plot(np.unique(accuracy_all_test), np.poly1d(np.polyfit( np.arange(len(accuracy_all_test)), accuracy_all_test, 1))(np.unique(accuracy_all_test)))
+#     plt.title(f'FG, Dividers {numbers_to_divide_by}, layers {layers}, full learning')
+#     plt.xlabel('epoch')
+#     plt.ylabel('accuracy')
+#     plt.legend(['train', 'test', "regression line"], loc='upper left')
+#     plt.savefig(f'FG, Dividers {numbers_to_divide_by}, layers {layers}, full learning.png')
+#     plt.show()
+#     del model
 
-            # Add nodes to the graph
-            for i in range(num_nodes):
-                G.add_node((layer.name, i))
-
-            # Add edges with weights to the graph
-            for i in range(num_nodes_prev):
-                for j in range(num_nodes):
-                    weight = W[i, j]
-                    G.add_edge((layer.name, i), (layer.name, j), weight=weight)
-
-    return G
-
-
-def model_crate(num_inputs, layers):
-    model = Sequential()
-    if layers == 0:
-        model.add(Dense(1, input_dim=num_inputs, activation='sigmoid'))
-    else:
-        model.add(Dense(num_inputs, input_dim=num_inputs, activation='relu'))
-        for i in range(layers - 1):
-            model.add(Dense(num_inputs, activation='relu'))
-        model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=[keras.metrics.BinaryAccuracy()])
-    plot_model(model, to_file=f'model_{layers}_layers.png', show_shapes=True, show_layer_names=True)
-    return model
-
-
-def MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model: Sequential, layers, total_epochs_for_each_label=300, round_epoch=10):
-    cnt = 0
-    accuracy_all_train = []
-    accuracy_all_test = []
-    accuracy_train = {}
-    accuracy_test = {}
-    for divider in numbers_to_divide_by:
-        accuracy_train[divider] = []
-        accuracy_test[divider] = []
-    while cnt < total_epochs_for_each_label:
-        for j, number in enumerate(numbers_to_divide_by):
-            cur_train = y_train.iloc[:, j]
-            cur_test = y_test.iloc[:, j]
-            model.fit(X_train, cur_train, validation_data=[X_test, cur_test], epochs=round_epoch, verbose=0, shuffle=True,
-                      use_multiprocessing=True, workers=4)
-            accuracy_all_train.extend(model.history.history['binary_accuracy'])
-            accuracy_all_test.extend(model.history.history['val_binary_accuracy'])
-            accuracy_train[number].extend(model.history.history['binary_accuracy'])
-            accuracy_test[number].extend(model.history.history['val_binary_accuracy'])
-        cnt += round_epoch
-        print(f'epoch {cnt} out of {total_epochs_for_each_label}')
-    plt.plot(accuracy_all_train)
-    plt.title(
-        f'MVG accuracy train, layers {layers}, round epoch {round_epoch}  {numbers_to_divide_by}')
-    plt.show()
-    plt.plot(accuracy_all_test)
-    plt.title(
-        f'MVG accuracy test, layers {layers}, round epoch {round_epoch}  {numbers_to_divide_by}')
-    plt.show()
-    for divider in numbers_to_divide_by:
-        plt.plot(accuracy_train[divider], label=f'train {divider}')
-        plt.plot(accuracy_test[divider], label=f'test {divider}')
-        plt.title(f'MVG, layers {layers}, round epoch {round_epoch}, divider {divider}')
-        plt.legend()
-        # save fig in folder named by date
-        plt.savefig(f'MVG, layers {layers}, round epoch {round_epoch}, divider {divider}.png')
-        plt.show()
-    # run the algorithm on the model
-    # check if the algorithm found the correct number of dividers
-    # check if the algorithm found the correct dividers
-
-
-def get_all_premonition(numbers, amount_of_elements):
-    combinations = list(itertools.combinations(numbers, amount_of_elements))
-    for i in range(len(combinations)):
-        combinations[i] = np.prod(combinations[i])
-    return combinations
-
-
-def FG(X_train, y_train, X_test, y_test, divider, model: Sequential, layers, iterations=100):
-    model.fit(X_train, y_train, epochs=iterations, validation_data=(X_test, y_test), shuffle=True, use_multiprocessing=True, workers=4)
-    # find the point where the accuracy is 0.9
-    accuracy_all_train = model.history.history['binary_accuracy']
-    accuracy_all_test = model.history.history['val_binary_accuracy']
-    point_where_accuracy_is_09 = -1
-    if accuracy_all_test[-1] > 0.9:
-        point_where_accuracy_is_09 = bisect.bisect_left(accuracy_all_test, 0.9)
-        print(f'point where accuracy is 0.9 is {point_where_accuracy_is_09}')
-    plt.plot(accuracy_all_train)
-    plt.plot(accuracy_all_test)
-    if point_where_accuracy_is_09 != -1:
-        plt.plot(point_where_accuracy_is_09, 0.9, 'ro')
-        plt.text(point_where_accuracy_is_09, 0.9, f'({point_where_accuracy_is_09}, 0.9)')
-    plt.title(f'FG, Divider {divider}, layers {layers}')
-    plt.xlabel('epoch')
-    plt.ylabel('accuracy')
-    plt.legend(['train', 'test'])
-    plt.savefig(f'FG, Divider {divider}, layers {layers}.png')
-    plt.show()
-
-
-from ann_visualizer.visualize import ann_viz
-
-
-def model_to_graph(model: Sequential):
-    dot_img_file = 'model_1.png'
-    ann_viz(model, title="My first neural network")
-    G = convert_to_graph(model)
-    print(G.nodes)
-    print(G.edges)
-    nx.draw(G)
-    plt.show()
-
-
-def main():
-    num_inputs = 32
-    sample_size = 20000
-    numbers_to_divide_by = get_all_premonition([3, 5, 7, 9], amount_of_elements=3)
-    max_number = 100000
-    # layers 3 - 10
-    # switch rate 2 - 20
-    # modularity
-    # start from one number
-    df = create_data(num_inputs, sample_size, max_number, numbers_to_divide_by, use_old_data=True)
-
-    df.drop_duplicates(inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    # print some statistics about the data
-    print(df['decimal'].describe())
-    print(f"amount of unique values: {df['decimal'].nunique()}, total amount of values: {len(df['decimal'])}")
-    X = df.filter(regex='input')
-    y = df.filter(regex='divide_by')
-    # find thw is the max number of bits in the input
-    # split the data into training and testing
-    y_test: DataFrame
-    y_train: DataFrame
-    X_test: DataFrame
-    X_train: DataFrame
-    X_train, X_test, y_train, y_test, = train_test_split(X, y, test_size=0.2, random_state=42)
-    # move to date folder
-    dir_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
-    os.chdir(dir_name)
-
-    # time_start = time.time()
-    for layers in range(6, 7):
-        clear_session()
-        model = model_crate(num_inputs, layers)
-        FG(X_train, y_train.iloc[:, -1], X_test, y_test.iloc[:, -1], numbers_to_divide_by[-1], model, layers, iterations=20)
-        model_to_graph(model)
-        del model
-    # time_end = time.time()
-    # print(f'time to run FG: {time_end - time_start}')
-    # time_start = time.time()
-
-    # for layers in range(2, 3):
-    #     clear_session()
-    #     model = model_crate(num_inputs, layers)
-    #     MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, layers, total_epochs_for_each_label=300, round_epoch=20)
-    #     del model
-    # time_end = time.time()
-    # print(f'time to run MVG: {time_end - time_start}')
-    # MVG(X_train, y_train, X_test, y_test, model, num_epochs, numbers_to_divide_by)
-
-
-if __name__ == '__main__':
-    main()
-
-    # FG(X_train, y_train.iloc[:, 0], X_test, y_test.iloc[:, 0], numbers_to_divide_by[0], model)
-    # MVG(X_train, y_train, X_test, y_test, model, num_epochs, numbers_to_divide_by)
-
-    # learn_on_one_divider(X_train, y_train, X_test, y_test, model, num_epochs, numbers_to_divide_by)
+# for layers in range(0, 7):
+#     clear_session()
+#     model = model_crate(num_inputs, layers)
+#     MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, layers, num_round=8, epoch_per_round=20)
+#     # evaluate_model(model, numbers_to_divide_by, 'MVG')
+#
+#     del model
+#
