9a09ad9378d7e37eaa8a72e88f889ce02fdb95ac
yaniv <qywoec@gmail.com>
2023-08-17 18:44:31 +0300

    power low weight choosing

--- main.py
+++ main.py
@@ -48,18 +48,28 @@ logger = logging.getLogger(__name__)
 def MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model: Sequential, num_round=11, epoch_per_round=10):
     accuracy_columns = ['round', 'epoch', 'divider', 'train_accuracy', 'test_accuracy']
     accuracy_df = pd.DataFrame(columns=accuracy_columns)
+
+    power_law_weights = np.random.power(a=2, size=len(y_train.columns))
+
+    # Normalize the weights
+    normalized_weights = power_law_weights / power_law_weights.sum()
+    normalized_weights = [0.3,0.2,0.5]
+    print(f"{numbers_to_divide_by[0]} : {normalized_weights[0]}")
+    print(f"{numbers_to_divide_by[1]} : {normalized_weights[1]}")
+    print(f"{numbers_to_divide_by[2]} : {normalized_weights[2]}")
     for i in range(num_round):
-        for j, number in enumerate(numbers_to_divide_by):
-            cur_train = y_train.iloc[:, j]
-            cur_test = y_test.iloc[:, j]
-            model.fit(X_train, cur_train, validation_data=[X_test, cur_test], epochs=epoch_per_round, verbose=1,
-                      shuffle=True,
-                      use_multiprocessing=True, workers=32)
-            accuracy_df = pd.concat([accuracy_df, pd.DataFrame({'round': i, 'epoch': np.arange(epoch_per_round), 'divider': number,
-                                                                'train_accuracy': model.history.history['binary_accuracy'],
-                                                                'test_accuracy': model.history.history['val_binary_accuracy']})],
-                                    ignore_index=True)
-            print(f'round {i} out of {num_round} divider {number}')
+
+        # Choose a column using the power-law distribution
+        choice = np.random.choice(y_train.columns, p=normalized_weights)
+        cur_train = y_train[choice]
+        cur_test = y_test[choice]
+        model.fit(X_train, cur_train, validation_data=[X_test, cur_test], epochs=epoch_per_round, verbose=1, shuffle=True,
+                  use_multiprocessing=True, workers=32)
+        accuracy_df = pd.concat([accuracy_df, pd.DataFrame({'round': i, 'epoch': np.arange(epoch_per_round), 'divider': choice,
+                                                            'train_accuracy': model.history.history['binary_accuracy'],
+                                                            'test_accuracy': model.history.history['val_binary_accuracy']})],
+                                ignore_index=True)
+        print(f'round {i} out of {num_round} divider {choice}')
         if i % 5 == 0:
             plt.plot(accuracy_df['train_accuracy'])
             plt.plot(accuracy_df['test_accuracy'])
@@ -67,14 +77,14 @@ def MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model: Sequentia
             plt.legend(['train', 'test'], loc='upper left')
             plt.savefig(os.path.join(cur_out_dir, f'MVG_sequential_step{i}.png'))
             plt.show()
-            for divider in numbers_to_divide_by:
+            for divider in y_test.columns:
                 divider_data = accuracy_df[accuracy_df['divider'] == divider]
                 plt.plot(divider_data['train_accuracy'].reset_index(drop=True), label=f'train {divider}')
                 plt.plot(divider_data['test_accuracy'].reset_index(drop=True), label=f'test {divider}')
             plt.title(f'MVG round {i}, separate graphs')
             plt.legend()
             plt.savefig(
-                os.path.join(cur_out_dir,f'MVG_separate_step{i}.png'))
+                os.path.join(cur_out_dir, f'MVG_separate_step{i}.png'))
             plt.show()
             accuracy_df.to_csv(os.path.join(cur_out_dir, f'MVG_data.csv'), index=False)
     accuracy_df.to_csv(os.path.join(cur_out_dir, f'MVG_data.csv'), index=False)
@@ -146,9 +156,9 @@ def main():
     amount_of_rounds_mvg = args.amount_of_rounds_mvg
     layers = args.layers
     width_multiplayer = args.width_multiplayer
-    modules = [3, 5, 11]
+    modules = [3, 7, 11]
     numbers_to_divide_by = get_all_premonition_mult(modules, amount_of_elements=2)
-    regularizer = l1(1e-5)
+    regularizer = l1(1e-6)
 
     logger.info(f'num_inputs: {num_inputs}')
     logger.info(f'data_size: {data_size}')
@@ -181,12 +191,14 @@ def main():
     plt.clf()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
     model = model_crate(num_inputs, layers, width_multiplayer=width_multiplayer, regularizer=regularizer)
+    if regularizer is not None:
+        logger.info(f'regularizer: {regularizer.get_config()}')
+    else:
+        logger.info(f'regularizer: None')
     log_with_separator("Model summary")
     logger.info(f'amount of layers: {layers}')
     logger.info(f'layers width: {width_multiplayer * num_inputs}')
-    logger.info(f'regularizer: {regularizer.get_config()}')
     model.summary(print_fn=logger.info)
     if learning_type == 'MVG':
         MVG(X_train, y_train, X_test, y_test, numbers_to_divide_by, model, num_round=amount_of_rounds_mvg,
@@ -201,7 +213,6 @@ if __name__ == '__main__':
     time = timeit(main, number=1)
     logger.info(f'time to run main: {time}')
     # os.rmdir(cur_out_dir)
-
 
 #     TODO:
 #     layers 3 - 10
